54aa01acd27eb22c72e8b329915dba519eefa287
Fabiano Beselga <fabianobeselga@gmail.com>
2017-09-17 11:01:39 -0300

    version route

--- app.rb
+++ app.rb
@@ -2,11 +2,17 @@ require 'sinatra'
 require 'bundler/setup'
 require 'redis'
 
+set :redis, Redis.new(url: ENV['REDIS_URL'])
+
 get '/' do
   'Hello Sinatra'
 end
 
 get '/ink' do
-  redis = Redis.new(url: ENV['REDIS_URL'])
-  redis.incr("counter").to_s
+  # redis = Redis.new(url: ENV['REDIS_URL'])
+  settings.redis.incr("counter").to_s
+end
+
+get '/version' do
+  ENV['HOSTNAME']
 end
